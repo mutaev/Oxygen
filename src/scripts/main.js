2731cbe1aa19e849f0fd0163347ddaa8387ad9dc
eldar mutaev <eldarmutaev@inbox.ru>
2022-09-12 20:25:43 +0300

    header

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+$(function () { 
+
+  $('.reviews__slider').slick({
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    dots: true,
+    arrows: true,
+    fade: true,
+    autoplay: true
+  });
+})
